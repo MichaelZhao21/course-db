68408fc789fae53e0e9896cb617718783b63a6bf
Michael Zhao <michaelzhao314@gmail.com>
2022-01-30 04:02:15 -0600

    Added framework for fronendtend fire

---
+++ client/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+
+import firebaseConfig from '../permissions';
+firebase.initializeApp(firebaseConfig);
+
+export async function uploadImage(img) {
+    const storage = getStorage();
+    const imgRef = ref(storage, img);
+    const storageRef = ref(storage, 'files');
+
+    const snap = await uploadBytes(storageRef, imgRef);
+    console.log('done!');
+}
