cbe81a8b23414606e59f1c9a34daff812d716a19
Michael Zhao <michaelzhao314@gmail.com>
2022-01-29 20:23:45 -0600

    Added mui and finished framework for app

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,15 @@
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import Logo from './logo';
 
 export default function Home(){
-
     return(
-        <div>
-
-        </div>
+        <Container>
+            <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                <Typography variant="h2" sx={{ flexBasis: 1, flexShrink: 0, flexGrow: 1 }}>Todo List</Typography>
+                <Logo fontSize="80px" sx={{ flexBasis: 1, flexShrink: 0, flexGrow: 1 }} />
+                <Typography variant="h2" sx={{ flexBasis: 1, flexShrink: 0, flexGrow: 1 }}>Courses</Typography>
+            </Box>
+        </Container>
     );
 }
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,24 @@
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
+import Logo from './logo';
 
 export default function Login(){
-
     return(
-        <div>
-            
-        </div>
+        <Container>
+            <Box sx={{ display: 'block',
+                        backgroundColor: theme => theme.palette.primary.main, 
+                        width: "50vw",
+                        height: "75vh",
+                        borderRadius: "30px"
+                        }}>
+                <Container sx={{ display: "flex",
+                        flexDirection: "column"    
+                        }}>
+                    <Logo/>
+                    <Typography variant="h1">courseDB</Typography>
+                    
+                </Container>
+            </Box>
+        </Container>
     );
 }
