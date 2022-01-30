d4471b863585806ea65d42b86ce57ea2f2186c6e
DenialHandrails <danielhonrales@gmail.com>
2022-01-29 21:59:20 -0600

    This login does not work

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,24 +1,27 @@
 import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
 import Logo from './logo';
+import { TextField, Button } from '@mui/material';
+import { useFormControl } from '@mui/material/FormControl';
 
 export default function Login(){
+
+    username = "";
+    password = "";
+    const tryLogin = (username, password) => {
+        if(getField(username) == "johnDoe" && getField(password) == "password"){
+            this.props.history.push('/');
+        }
+    };
+
     return(
         <Container>
-            <Box sx={{ display: 'block',
-                        backgroundColor: theme => theme.palette.primary.main, 
-                        width: "50vw",
-                        height: "75vh",
-                        margin: "auto",
-                        borderRadius: "30px"
-                        }}>
-                <Container sx={{ display: "flex",
-                        flexDirection: "column"    
-                        }}>
-                    <Logo/>
-                    <Typography variant="h1">courseDB</Typography>
-                    
-                </Container>
+            <Box sx={{ display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center'}}>
+                <Logo sx={{width: "10%", height: "10%", margin: "7.5% auto auto auto"}}/>
+                <Typography variant="h2" sx={{margin:"5% auto auto auto"}}>Login to courseDB</Typography>
+                <TextField id="usernameField" label="Username" variant="standard" onChange={} sx={{margin:"5% auto auto auto"}}/>
+                <TextField id="passwordField" label="Password" variant="standard" onChange={} />
+                <Button variant="text" onClick={tryLogin(usernameField, passwordField)} sx={{margin:"2.5% auto auto auto"}}>Submit</Button>
             </Box>
         </Container>
     );
