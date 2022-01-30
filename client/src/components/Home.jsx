89945e5e997f11fc3c056c9ee68a60a6b7e5955f
Michael Zhao <michaelzhao314@gmail.com>
2022-01-29 20:30:58 -0600

    Recovered Home file

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,10 +2,23 @@ import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
 import Logo from './logo';
 
-export default function Home(){
+export default function Login(){
     return(
-        <div>
-            
-        </div>
+        <Container>
+            <Box sx={{ display: 'block',
+                        backgroundColor: theme => theme.palette.primary.main, 
+                        width: "50vw",
+                        height: "75vh",
+                        borderRadius: "30px"
+                        }}>
+                <Container sx={{ display: "flex",
+                        flexDirection: "column"    
+                        }}>
+                    <Logo/>
+                    <Typography variant="h1">courseDB</Typography>
+                    
+                </Container>
+            </Box>
+        </Container>
     );
 }
