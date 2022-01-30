5cfd3e62ff1c1ee6ef90392ff36ed4ba38594511
Daniel Honrales <danielhonrales@gmail.com>
2022-01-29 23:26:15 -0600

    Added course page

---
+++ client/src/components/Course.jsx
@@ -0,0 +1,13 @@
+import { Box, List, Typography } from '@mui/material';
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import CourseListEntry from './CourseListEntry';
+import Logo from './logo';
+
+export default function Home() {
+
+    return{
+        
+    }
+}
