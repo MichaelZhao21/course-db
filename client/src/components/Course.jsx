e370ef60e0fe99d5ea072248f0e8b63c4e2bf6ca
Daniel Honrales <danielhonrales@gmail.com>
2022-01-30 02:10:37 -0600

    Course Bones Done

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Logo from './logo';
+import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
@@ -41,20 +43,33 @@ export default function Course() {
 
     return(
         <Container>
-            <Logo sx={{position: "absolute", top: 10, left: 10}}/>
-            <Box sx={{display:"flex", flexDirection: "column", height:"12vh", margin: "4% auto auto auto"}}>
-                
-                <Typography variant="h3" sx={{}}> {name} </Typography>
-                <Typography variant="h4" sx={{}}> {prof} </Typography>
+            <Box sx={{display:"flex", flexDirection: "row", height:"9vh", marginTop: "4%"}}>
+                <Logo sx={{marginRight: "4% ", fontSize:"75px"}}/>
+                <Box sx={{display:"flex", flexDirection: "column"}}>
+                    <Typography variant="h3" sx={{fontSize: "210%"}}> {name} </Typography>
+                    <Typography variant="h2" sx={{fontSize: "190%", marginTop: "1%"}}> {prof} </Typography>
+                </Box>
             </Box>
 
-            <Box sx={{display:"flex", width: "100%", height:"12vh"}}>
-                <Button variant="text" sx={{width: "30px"}}>Todo</Button>
-                <Button variant="text" sx={{width: "30px"}}>Lectures</Button>
-                <Button variant="text" sx={{width: "30px"}}>Notes</Button>
-                <Button variant="text" sx={{width: "30px"}}>Homework</Button>
+            <Box sx={{display:"flex", width: "100%", height:"12vh", marginTop: "4%"}}>
+                <Grid container spacing={2}>
+                    <Grid item xs={3}>
+                        <Button variant="text" sx={{width: "300px"}}>Todo</Button>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant="text" sx={{width: "300px"}}>Lectures</Button>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant="text" sx={{width: "300px"}}>Notes</Button>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button variant="text" sx={{width: "300px"}}>Homework</Button>
+                    </Grid>
+                </Grid>
             </Box>
-            
+
+            <Divider sx={{marginTop:"-4%"}}/>
+
             <Box>
 
             </Box>
