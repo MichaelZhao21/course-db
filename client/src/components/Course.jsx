354322d236758ebd22528832b7ca527e5824090d
Daniel Honrales <danielhonrales@gmail.com>
2022-01-29 23:29:37 -0600

    Course

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -1,13 +1,15 @@
-import { Box, List, Typography } from '@mui/material';
+import { Container, Box, List, Typography } from '@mui/material';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import CourseListEntry from './CourseListEntry';
 import Logo from './logo';
 
-export default function Home() {
+export default function Course() {
 
-    return{
-        
-    }
+    return(
+        <Container>
+wq
+        </Container>
+    )
 }
