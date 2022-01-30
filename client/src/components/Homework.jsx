fddafb6903fa040a56763d0800c77d2bed9a06f1
Michael Zhao <michaelzhao314@gmail.com>
2022-01-30 02:19:30 -0600

    Create file display icon

--- client/src/components/Homework.jsx
+++ client/src/components/Homework.jsx
@@ -1,14 +1,17 @@
-import { Container, Box, List, Typography } from '@mui/material';
+import { Container, Box, List, Typography, Grid } from '@mui/material';
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import FileDisplay from './FileDisplay';
 import Logo from './logo';
 
 export default function Homework() {
 
     return(
-        <Box>
-            
-        </Box>
+        <Grid container spacing={2}>
+            <Grid item>
+                <FileDisplay ext="pdf" name="HW1 - CS 3345" />
+            </Grid>
+        </Grid>
     )
 }
